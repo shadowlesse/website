f198915872507b6978ad29b0288f05d032194709
shadowlesse <shadowlesse@163.com>
2016-06-04 14:20:35 +0800

    hh

--- JackDaniels/js/index.js
+++ JackDaniels/js/index.js
@@ -16,7 +16,18 @@ $(document).ready(function(e) {
 				direction:'left'
 			});
 	
-
+	
+	var goL_small_img = $('#goL_small img');
+	var goR_small_img = $('#goR_small img');
+	goL_small_img.mousedown(function(){
+			$(this).attr("src","images/arrow_07_1.jpg");
+			goR_small_img.attr("src","images/arrow_08_1.jpg");
+		});
+	goR_small_img.mousedown(function(){
+			$(this).attr("src","images/arrow_08.jpg");
+			goL_small_img.attr("src","images/arrow_07.jpg");
+		});
+		
 			
 	$('.fancybox').fancybox();
 	
