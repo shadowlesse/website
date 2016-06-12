a8db0ffffd4aabf725a0e14a230a7440d3cdfba3
shadowlesse <shadowlesse@163.com>
2016-06-13 00:48:35 +0800

    up

--- PersonalSite/js/index.js
+++ PersonalSite/js/index.js
@@ -3,22 +3,15 @@
 
 $(document).ready(function(e) {
    $('.navigation>li').click(function(){
-   	$('.navigation>li>a').removeClass();
-	$(this).find('a').addClass('text');
-   });
+   	$('.navigation>li>a').removeClass()
+	$(this).find('a').addClass('text')
+   })
 
 	arr = $('.left li p');
 
 	for(var i = 0, l = arr.length; i < l; i++) { 
-		addElement(arr[i].id, parseInt(arr[i].innerHTML));
+		$(arr[i]).mouseover(updateNumber(i,parseInt(arr[i].innerHTML)));
 	}
-
-	$('.left').mouseover(function(e){
-		if(e.target.nodeName=="P"){
-			up(e.target.id);
-		}
-	});
-
 
 	$("ul.small li img").mousedown(function(){
 		$("ul.small li img").removeClass('pic');
@@ -44,23 +37,16 @@ $(document).ready(function(e) {
 		};
 });
 
-function up(id){
-	n = dc[id];
-	ele = $("#"+id).get(0);
-	var num = parseInt(ele.innerHTML);
+
+function updateNumber(element,n){
+	var ps = arr[element];
+	var num = parseInt(ps.innerHTML);
 	if(num==n){
 		num=0;
 	}
-	ele.innerHTML = num + 1;
+	ps.innerHTML = num + 1;
 	if(num < n-1){
-		window.setTimeout("up('"+ id + "')",2);
+		window.setTimeout("updateNumber("+element+"," + n + ")",2);
 	}
-}
-
-
-dc={};
-
-function addElement(id,n){
-	dc[id]=n;
 }
 
