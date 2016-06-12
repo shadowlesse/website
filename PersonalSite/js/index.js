f6ab5dcf9d93da442930b8430e271b23a1c9d35b
shadowlesse <shadowlesse@163.com>
2016-06-12 23:48:27 +0800

    u

--- PersonalSite/js/index.js
+++ PersonalSite/js/index.js
@@ -26,13 +26,13 @@ $(document).ready(function(e) {
 	var dic = {
 		"1": {"img":"images/pics_01.jpg", "title":"Graphic Design", "desc":
 			'Graphic design is the process of visual communication, and problem-solving through the correct use of typography, space, image and color. The field is considered a subset of visual communication and communication design, but sometimes the term "graphic design" is used interchangeably with these due to overlapping skills involved. Graphic design often refers to both the process (designing) by which the communication is created and the products (designs) which are generated.'},
-		"2": {"img":"images/pics_03.jpg", "title":"Graphic Design2", "desc":
+		"2": {"img":"images/pics_03.jpg", "title":"Animation", "desc":
 			'2Graphic design is the process of visual communication, and problem-solving through the correct use of typography, space, image and color. The field is considered a subset of visual communication and communication design, but sometimes the term "graphic design" is used interchangeably with these due to overlapping skills involved. Graphic design often refers to both the process (designing) by which the communication is created and the products (designs) which are generated.'},
-		"3": {"img":"images/pics_06.jpg", "title":"Graphic Design3", "desc":
+		"3": {"img":"images/pics_06.jpg", "title":"Photography", "desc":
 			'3Graphic design is the process of visual communication, and problem-solving through the correct use of typography, space, image and color. The field is considered a subset of visual communication and communication design, but sometimes the term "graphic design" is used interchangeably with these due to overlapping skills involved. Graphic design often refers to both the process (designing) by which the communication is created and the products (designs) which are generated.'},
-		"4": {"img":"images/pics_08.jpg", "title":"Graphic Design4", "desc":
+		"4": {"img":"images/pics_08.jpg", "title":"Drawing", "desc":
 			'4Graphic design is the process of visual communication, and problem-solving through the correct use of typography, space, image and color. The field is considered a subset of visual communication and communication design, but sometimes the term "graphic design" is used interchangeably with these due to overlapping skills involved. Graphic design often refers to both the process (designing) by which the communication is created and the products (designs) which are generated.'},
-		"5": {"img":"images/pics_10.jpg", "title":"Graphic Design5", "desc":
+		"5": {"img":"images/pics_10.jpg", "title":"Mobile Design", "desc":
 			'5Graphic design is the process of visual communication, and problem-solving through the correct use of typography, space, image and color. The field is considered a subset of visual communication and communication design, but sometimes the term "graphic design" is used interchangeably with these due to overlapping skills involved. Graphic design often refers to both the process (designing) by which the communication is created and the products (designs) which are generated.'}
 		};
 });
