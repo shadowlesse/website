c2826602556d73e3159a2f33dfe7d60b9c1027d1
太禅 <taichan@taobao.com>
2016-06-12 22:34:19 +0800

    add

--- PersonalSite/js/index.js
+++ PersonalSite/js/index.js
@@ -6,4 +6,25 @@ $(document).ready(function(e) {
    	$('.navigation>li>a').removeClass()
 	$(this).find('a').addClass('text')
    })
+
+	arr = $('.left li p');
+
+	for(var i = 0, l = arr.length; i < l; i++) { 
+		$(arr[i]).mouseover(updateNumber(i,parseInt(arr[i].innerHTML)));
+	}
+
 });
+
+
+function updateNumber(element,n){
+	var ps = arr[element];
+	var num = parseInt(ps.innerHTML);
+	if(num==n){
+		num=0;
+	}
+	ps.innerHTML = num + 1;
+	if(num < n-1){
+		window.setTimeout("updateNumber("+element+"," + n + ")",2);
+	}
+}
+
